7b906d19aaf9f210ecc6bbf8b260bf76926c55dc
Adolfo <yml.aldana@gmail.com>
2020-07-22 16:28:29 -0400

    creacion del componente header y agregado titulo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
+  const titulo = 'Cotizador de Seguros'
   return (
     <Fragment>
-
+      <Header titulo={titulo}/>
     </Fragment>
   );
 }
