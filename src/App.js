9c2f2063765c7a65bdee1dd2ad61d9ee76b0a75a
Adolfo <yml.aldana@gmail.com>
2020-07-22 20:34:56 -0400

    uso del estilo emotion styled en App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Fragment } from 'react';
 import './App.css';
 import Header from './components/Header';
+import styles from '@emotion/styled';
+
+const Contenedor = styled.div`
+max-width: 600px;
+margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+background-color: #FFF;
+padding: 3rem;
+`;
 
 function App() {
   const titulo = 'Cotizador de Seguros'
   return (
-    <Fragment>
-      <Header titulo={titulo}/>
-    </Fragment>
+    <Contenedor>
+      <Header titulo={titulo} />
+
+      <ContenedorFormulario>
+        
+      </ContenedorFormulario>
+    </Contenedor>
   );
 }
 
